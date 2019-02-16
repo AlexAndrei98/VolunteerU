0539eec481fc206e0b25637cbaba9905d09249ec
Marwan Helali <marwan01@Marwans-MBP.dhcp.umsl.edu>
2019-02-16 10:41:09 -0600

    router setup with a button
    good progress

--- volunteeru/src/components/dashboard.vue
+++ volunteeru/src/components/dashboard.vue
@@ -1,6 +1,12 @@
 <template>
 <div id="dashboard">
     <h3> Dashboard </h3>
+
+    <div class="fixed-action-btn">
+        <router-link to="/new" class = "btn-floating btn-large green">
+            <i class ="fa fa-plus"></i>
+        </router-link>
+        </div>
     </div>
 </template>
     <script>
