186d69851d70d62b4435e5db16997322caa4c889
Shraddha Belbase <shraddhabelbase@Shraddhas-MBP.dhcp.umsl.edu>
2019-02-16 19:05:11 -0600

    started implementing avatar and title and points in header menu

--- volunteeru/src/components/Navbar.vue
+++ volunteeru/src/components/Navbar.vue
@@ -1,9 +1,25 @@
  <template>
-  <nav>
-      <div class="green">
+ 
+<div class="green">
         <div class="container">
-          <router-link to="/" style="    font-size: 40px;">VolunteerU</router-link>    
+          <v-avatar>
+        <img
+          src="https://cdn.vuetifyjs.com/images/john.jpg"
+          alt="John"
+        >
+      </v-avatar>
+       <v-toolbar-title class="white--text">User</v-toolbar-title>
+
+          <router-link to="/" style="    font-size: 40px; align: center">VolunteerU</router-link>  
+       
+
+    <v-btn icon>
+      <v-icon>search</v-icon>
+    </v-btn>
+
+
         </div>
-      </div>
-    </nav>
+        </div>
+    
   </template>
+  
